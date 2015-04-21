3e3cc769b7e775fd4c2640de19fabf2908fb7ec4
楠殷 <Asia_CRM@nanyindeMacBook-Pro.local>
2015-04-21 16:37:21 +0800

    Initial Commit

---
+++ learnC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  learnC
+//
+//  Created by 楠殷 on 15/4/21.
+//  Copyright (c) 2015年 ian. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
